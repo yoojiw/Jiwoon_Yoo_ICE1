fbcc46b2b709baf34bbab2b24ffaf0e523254424
yoojiw <yoojiw@sheridancollege.ca>
2022-02-02 11:04:24 -0500

    added new class PartTimeStudentvv

---
+++ src/ca/sheridancollege/week2/softwarefundamentals/SecondBranchClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week2.softwarefundamentals;
+
+/**
+ *
+ * @author irene
+ */
+public class SecondBranchClass extends CardDealer{
+     //Now create one more object of Card
+          //Now create one more object of Card
+        Card two = new Card("hearts", 4);
+        
+}
